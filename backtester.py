6235ba12bd81bf2c04da0a31a74428946dae5722
Rizwan <58824788+rizwan236@users.noreply.github.com>
2026-02-11 19:19:00 +0530

    Update backtester.py

--- backtester.py
+++ backtester.py
@@ -96,12 +96,15 @@ def calculate_trades(df, buy_rsi, buy_cci, sell_rsi, sell_cci):
         weighted_excessMR =float(df['weighted_excessMR'].iloc[i])
         #ST =pd.Series(data['ST'].values.flatten(), index=data.index) 
         DD_LOG =float(df['DD_LOG'].iloc[i])
+        RSI_max = float(df['RSI_max'].iloc[i])
+        RSI_min = float(df['RSI_min'].iloc[i])
+        
         
         
 
         #if ctx.bars >= 250 and ctx.indicator("HT_TRENDMODE")[-1] >= 1 and ctx.indicator("LINEARREG_SLOPE_OBV")[-1] > 2 and ctx.niftyClose[-1] > ctx.indicator("niftyClosewkH52")[-1] * 0.0 and ctx.niftyClose[-1]*100 > ctx.indicator("SMAniftyClose10")[-1]*10 > ctx.indicator("SMAniftyClose30")[-1]*0 and ctx.indicator("SMAAD")[-2] * 1.0 < ctx.AD[-1] and ctx.indicator("ADX")[-3] < ctx.indicator("ADX")[-1] > 25 and ctx.indicator("PLUS_DI")[-1] > ctx.indicator("PLUS_DI")[-3] and (ctx.indicator("PLUS_DI")[-1] - ctx.indicator("MINUS_DI")[-1]) > 15 and ctx.volume[-90] >= 1000 and ctx.volume[-30] >= 1000 and ctx.volume[-3] >= 1000 and (0 != ctx.MOMScore[-1] > 1.5 or 0 != ctx.weighted_excessMR[-1] > 0.4) and ctx.indicator("SMAScore24")[-1] > -10 and (ctx.ST[-1] == 1 and ctx.indicator("EMAMRP24")[-1] < ctx.MRP[-1] and ctx.indicator("EMAOBV")[-1] * 1.0 < ctx.OBV[-1] and ctx.close[-1] > ctx.indicator("SMAClose10")[-1] > ctx.indicator("SMAClose30")[-1] > ctx.indicator("SMAClose40")[-1] and ctx.indicator("SMAClose40")[-21] < ctx.indicator("SMAClose40")[-1] > ctx.indicator("sma_based_sma200")[-1] and ctx.indicator("RSI_14")[-1] > 50 and ctx.close[-1] > ctx.indicator("wkH52")[-1] * .75 and ctx.close[-1] > ctx.indicator("wkL52")[-1] * 1.35):
         #if not in_position and rsi_val > buy_rsi and cci_val > buy_cci  and ST ==1:
-        if not in_position and  SMAAD2 * 1.0 < AD and ADX3 < ADX > 25 and PLUS_DI > PLUS_DI3 and (PLUS_DI- MINUS_DI) > 15 and volume_val >= 500 and (0 != MOMScore > 1.5 or 0 != weighted_excessMR > 0.4) and SMAScore24 > -10 and (ST == 1 and EMAMRP24 < MRP and EMAOBV * 1.0 < OBV and close_val > SMAClose10 > SMAClose30 > SMAClose40 and SMAClose40_21 < SMAClose40 > sma_based_sma200 and RSI_e > 50 and close_val > wkH52 * .75 and close_val > wkL52 * 1.35):    
+        if not in_position and  SMAAD2 * 1.0 < AD and ADX3 < ADX > 25 and PLUS_DI > PLUS_DI3 and (PLUS_DI- MINUS_DI) > 15 and volume_val >= 500 and (0 != MOMScore > 1.5 or 0 != weighted_excessMR > 0.4) and SMAScore24 > -10 and (ST == 1 and EMAMRP24 < MRP and EMAOBV * 1.0 < OBV and close_val > SMAClose10 > SMAClose30 > SMAClose40 and SMAClose40_21 < SMAClose40 > sma_based_sma200 and RSI_e > 50 < RSI_min and RSI_max > 65 and close_val > wkH52 * .75 and close_val > wkL52 * 1.35):    
             in_position, entry_price, entry_date = True, close_val, date
             df.loc[date, 'Signal'] = 'BUY'
         #if (ctx.bars >= 250 and (ctx.close[-1] < ctx.indicator("SMAClose40")[-1] and ctx.ST[-1] == -1 and ctx.DD_LOG[-1] > 15)) or ((0 != ctx.MOMScore[-1] < 1.5 or 0 != ctx.weighted_excessMR[-1] < 0.4 or ctx.DD_LOG[-1] > 25 or ctx.indicator("EMAOBV")[-1] * 0.95 > ctx.OBV[-1]) and ctx.ST[-1] == -1 and ctx.DD_LOG[-1] > 15 and (ctx.indicator("RSI_20")[-1] < 35 or ctx.indicator("CCI_34")[-1] < 0 or ctx.indicator("SMAClose10")[-1] < ctx.indicator("SMAClose30")[-1])):    
